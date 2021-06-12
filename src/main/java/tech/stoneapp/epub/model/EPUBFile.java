172475f613bfb793f37fc9573d44e9981e08e689
Tony Yang <t510599@gmail.com>
2021-06-13 01:39:22 +0800

    [Update] EPUBFile model

--- src/main/java/tech/stoneapp/epub/model/EPUBFile.java
+++ src/main/java/tech/stoneapp/epub/model/EPUBFile.java
@@ -1,4 +1,43 @@
 package tech.stoneapp.epub.model;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipFile;
+
+import java.io.*;
+
 public class EPUBFile {
+    private String filename;
+    private String path;
+    private File file;
+    private ConvertStatus status = ConvertStatus.PENDING;
+
+    public EPUBFile(String filepath) throws FileNotFoundException {
+        this.file = new File(filepath);
+        if (!this.file.exists()) {
+            throw new FileNotFoundException();
+        }
+
+        this.path = this.file.getAbsolutePath();
+        this.filename = this.file.getName();
+    }
+
+    public void updateStatus(ConvertStatus status) {
+        this.status = status;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public ConvertStatus getStatus() {
+        return status;
+    }
 }
