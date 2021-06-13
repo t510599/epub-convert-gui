3d323b457ce8f8240b575ecad95e93b713ac4d1c
Tony Yang <t510599@gmail.com>
2021-06-13 19:03:31 +0800

    [Feature] Basic command app done

--- src/main/java/tech/stoneapp/epub/cli/CommandLineApp.java
+++ src/main/java/tech/stoneapp/epub/cli/CommandLineApp.java
@@ -1,60 +1,38 @@
 package tech.stoneapp.epub.cli;
 
 import java.io.*;
-import java.util.Enumeration;
 
-import com.github.houbb.opencc4j.util.ZhConverterUtil;
-import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.compress.archivers.zip.ZipFile;
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-
-import org.apache.commons.compress.archivers.ArchiveOutputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import tech.stoneapp.epub.convertor.EPUBConvertor;
+import tech.stoneapp.epub.exception.NotEPUBException;
+import tech.stoneapp.epub.model.EPUBFile;
 
 public class CommandLineApp {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-        for (String filename: args) {
-            ByteArrayOutputStream newMemoryFile = new ByteArrayOutputStream();
+        EPUBConvertor convertor = EPUBConvertor.getInstance();
+
+        for (String filepath: args) {
+            long startTime = System.currentTimeMillis();
+
             try {
-                File f = new File(filename);
-                String result = ZhConverterUtil.toTraditional(filename);
-                System.out.printf("%s, size: %d\n", result, f.length());
+                EPUBFile epub = new EPUBFile(filepath);
+                convertor.convert(epub);
 
-                ZipFile epub = new ZipFile(f);
-                ZipArchiveEntry entry;
-//                OutputStream newFile = new FileOutputStream(result);
-                ArchiveOutputStream newZip = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, newMemoryFile);
-
-                Enumeration<ZipArchiveEntry> entries = epub.getEntries();
-                while (entries.hasMoreElements()) {
-                    entry = entries.nextElement();
-                    InputStream stream = epub.getInputStream(entry);
-                    newZip.putArchiveEntry(new ZipArchiveEntry(ZhConverterUtil.toTraditional(entry.getName())));
-                    IOUtils.copy(stream, newZip);
-                    newZip.closeArchiveEntry();
-                    System.out.printf("[%s] %s size:%s\n", result, entry.getName(), entry.getSize());
+                long currentTime = System.currentTimeMillis();
+                String message = "";
+                switch (epub.getStatus()) {
+                    case PENDING -> message = "File %s has not been converted.";
+                    case SUCCESS -> message = "%s has been converted successfully.";
+                    case FAILED -> message = "Conversion of %s has failed.";
+                    default -> message = "Unknown Error for %s.";
                 }
-
-                newZip.finish();
-                newMemoryFile.close();
-            } catch (IOException e) {
+                message = String.format(message, epub.getFilename());
+                System.out.printf("%s Elapsed time: %.2f s%n", message, (float)(currentTime - startTime) / 1000);
+            } catch (IOException | ArchiveException e) {
                 e.printStackTrace();
-            } catch (ArchiveException e) {
-                e.printStackTrace();
+            } catch (NotEPUBException e) {
+                System.out.printf("%s is not an EPUB. Skipped.%n", filepath);
             }
-
-
-            try {
-                OutputStream newFile = new FileOutputStream(ZhConverterUtil.toTraditional(filename));
-                newFile.write(newMemoryFile.toByteArray());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
         }
     }
 }
