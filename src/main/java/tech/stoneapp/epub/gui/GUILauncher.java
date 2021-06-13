14cfa32f79aba5fb849d26fb3bbef6e99c0464fc
Tony Yang <t510599@gmail.com>
2021-06-14 00:25:34 +0800

    [Update] implement showSettings() in GUILauncher

--- src/main/java/tech/stoneapp/epub/gui/GUILauncher.java
+++ src/main/java/tech/stoneapp/epub/gui/GUILauncher.java
@@ -9,8 +9,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class GUILauncher extends Application {
-    private final int screenWidth = 720;
-    private final int screenHeight = 540;
+    private static final int screenWidth = 720;
+    private static final int screenHeight = 540;
     private static Stage mainStage;
     private static Stage settingsStage;
 
@@ -29,8 +29,18 @@ public class GUILauncher extends Application {
         }
     }
 
-    public void showSettings() {
+    public static void showSettings() {
         settingsStage = new Stage();
+        try {
+            Parent root = FXMLLoader.load(GUILauncher.class.getResource("fxml/settings.fxml"));
+            Scene scene = new Scene(root, screenWidth * 2 / 3, screenHeight * 2 / 3);
+
+            settingsStage.setTitle("Settings");
+            settingsStage.setScene(scene);
+            settingsStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Stage getMainStage() {
