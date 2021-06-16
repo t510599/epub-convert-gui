276306fb242fead0115bd3ff5de3ee220ab421f9
Tony Yang <t510599@gmail.com>
2021-06-16 23:44:26 +0800

    [Update] remove unused HostService in GUILauncher

--- src/main/java/tech/stoneapp/epub/gui/GUILauncher.java
+++ src/main/java/tech/stoneapp/epub/gui/GUILauncher.java
@@ -1,7 +1,6 @@
 package tech.stoneapp.epub.gui;
 
 import javafx.application.Application;
-import javafx.application.HostServices;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,11 +13,9 @@ public class GUILauncher extends Application {
     private static final int screenHeight = 540;
     private static Stage mainStage;
     private static Stage settingsStage;
-    private static HostServices host;
 
     @Override
     public void start(Stage stage) {
-        host = getHostServices();
         try {
             mainStage = stage;
             Parent root = FXMLLoader.load(getClass().getResource("fxml/app.fxml"));
@@ -48,10 +45,6 @@ public class GUILauncher extends Application {
 
     public static Stage getMainStage() {
         return mainStage;
-    }
-
-    public static HostServices getHost() {
-        return host;
     }
 
     public static void main(String[] args) {
