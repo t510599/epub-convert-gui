83756f40c4bd5caa5e3b11b3ddb46b33122a0ba0
Tony Yang <t510599@gmail.com>
2021-06-17 18:56:35 +0800

    [Feature] select output directory

--- src/main/java/tech/stoneapp/epub/gui/controller/SettingsController.java
+++ src/main/java/tech/stoneapp/epub/gui/controller/SettingsController.java
@@ -4,10 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+
 import tech.stoneapp.epub.gui.GUILauncher;
 import tech.stoneapp.epub.model.AppConfig;
 import tech.stoneapp.epub.model.AppState;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,6 +34,28 @@ public class SettingsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        String outputDirectory = config.getOutputDirectory();
+        File directory = new File(outputDirectory);
+        if (outputDirectory.equals("") || !directory.exists()) {
+            path.selectToggle(path.getToggles().get(0));
+        } else {
+            path.selectToggle(path.getToggles().get(1));
+            pathInput.setText(outputDirectory);
+        }
+        selectPathButton.setOnMouseClicked(ev -> {
+            DirectoryChooser chooser = new DirectoryChooser();
+            chooser.setTitle("Choose output directory...");
+            File dir = chooser.showDialog(GUILauncher.getSettingsStage());
+
+            if (dir == null) return;
+
+            if (!dir.exists()) {
+                return;
+            }
+            pathInput.setText(dir.getAbsolutePath());
+        });
+        selectPathButton.disableProperty().bind(path.getToggles().get(0).selectedProperty());
+
         filenameChoiceBox.getItems().addAll(
                 AppConfig.OutputFilenameMode.TRANSLATE,
                 AppConfig.OutputFilenameMode.SUFFIX,
