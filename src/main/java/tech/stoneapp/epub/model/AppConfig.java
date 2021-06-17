a052a2a8af83cccd84a72a01d6578e3e584cb437
Tony Yang <t510599@gmail.com>
2021-06-17 16:31:46 +0800

    [Add] AppConfig

--- src/main/java/tech/stoneapp/epub/model/AppConfig.java
+++ src/main/java/tech/stoneapp/epub/model/AppConfig.java
@@ -1,4 +1,42 @@
 package tech.stoneapp.epub.model;
 
+import javafx.scene.control.Alert;
+
 public class AppConfig {
+    // empty stands for saving to input file's directory
+    private String outputDirectory = "";
+    private OutputFilenameMode outputFilenameMode = OutputFilenameMode.TRANSLATE;
+    private boolean overwrite = true;
+    private Alert.AlertType alertLevel = Alert.AlertType.INFORMATION;
+
+    public AppConfig() {}
+
+    public OutputFilenameMode getOutputFilenameMode() {
+        return outputFilenameMode;
+    }
+
+    public static AppConfig loadConfig() {
+        return new AppConfig();
+    }
+
+    public enum OutputFilenameMode {
+        /**
+         * TRANSLATE: Convert filename to Tranditional Chinese by opencc4j
+         * SUFFIX: Add "-tc" suffix to filename
+         * BOTH: Translate, then add suffix
+         */
+        TRANSLATE("Translate to Tranditional Chinese"),
+        SUFFIX("Add `-tc` suffix"),
+        BOTH("Both");
+
+        String description;
+        OutputFilenameMode(String description) {
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return description;
+        }
+    }
 }
