c7f1d5ee6a8090eee80503c42d29c935b0d7014c
Tony Yang <t510599@gmail.com>
2021-06-17 16:37:41 +0800

    [Add] SettingsController basic structure

--- src/main/java/tech/stoneapp/epub/gui/controller/SettingsController.java
+++ src/main/java/tech/stoneapp/epub/gui/controller/SettingsController.java
@@ -1,4 +1,37 @@
 package tech.stoneapp.epub.gui.controller;
 
-public class SettingsController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import tech.stoneapp.epub.gui.GUILauncher;
+import tech.stoneapp.epub.model.AppConfig;
+import tech.stoneapp.epub.model.AppState;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingsController implements Initializable {
+    @FXML private VBox root;
+
+    @FXML private ToggleGroup path;
+    @FXML private TextField pathInput;
+    @FXML private Button selectPathButton;
+
+    @FXML private ChoiceBox<AppConfig.OutputFilenameMode> filenameChoiceBox;
+    @FXML private CheckBox overwriteCheckbox;
+
+    @FXML private ChoiceBox alertLevel;
+
+    @FXML private Button saveButton;
+    @FXML private Button cancelButton;
+
+    private AppState state = GUILauncher.getState();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cancelButton.setOnMouseClicked(ev -> {
+            GUILauncher.getSettingsStage().close();
+        });
+    }
 }
